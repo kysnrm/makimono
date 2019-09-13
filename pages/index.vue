b5240fb103364b5c425fa3e5395441d9506af960
kysn <kysn.rm@gmail.com>
2019-09-14 08:26:46 +0900

    add form

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="container">
+    <form>
+      <label for="author">名前</label>
+      <input type="text" id="author" name="author" />
+      <label for="text">本文</label>
+      <input type="text" id="text" name="text" />
+      <input type="submit" value="投稿する" />
+    </form>
     <div v-for="(item, index) in items" :key="index">
       {{ item.author }}
       {{ item.text }}
