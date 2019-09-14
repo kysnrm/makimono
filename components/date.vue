126fb83f9c582785ea5f8ab5a5356a5ec060f7ce
kysn <kysn.rm@gmail.com>
2019-09-14 14:14:28 +0900

    devide date as component

--- components/date.vue
+++ components/date.vue
@@ -1,39 +1,11 @@
 <template>
-  <div class="container">
-    <form @submit.prevent="addItem(newPost.author, newPost.text)">
-      <label for="author">名前</label>
-      <input id="author" v-model="newPost.author" type="text" name="author" />
-      <label for="text">本文</label>
-      <input id="text" v-model="newPost.text" type="text" name="text" />
-      <input type="submit" value="投稿する" />
-    </form>
-    <div v-for="(item, index) in items" :key="index">
-      {{ item.author }}
-      {{ item.text }}
-      <kanjinized-date :date="item.createdAt"></kanjinized-date>
-    </div>
-  </div>
+  <div>{{ dateKanjinizer(date) }}</div>
 </template>
 
 <script>
-import kanjinizedDate from '../components/date'
 export default {
-  components: {
-    'kanjinized-date': kanjinizedDate
-  },
-  data: () => {
-    return {
-      newPost: { author: '', text: '' },
-      items: []
-    }
-  },
+  props: { date: Date },
   methods: {
-    addItem(author, text) {
-      if (author !== '' && text !== '') {
-        this.items.push({ author, text, createdAt: new Date() })
-        this.newPost.text = ''
-      }
-    },
     dateKanjinizer(createdAt) {
       const kanjinizedNum = [
         '〇',
@@ -77,5 +49,3 @@ export default {
   }
 }
 </script>
-
-<style></style>
