55168445ed5ae99d266af3167ae7189ae461d30d
kysn <kysn.rm@gmail.com>
2019-09-14 11:44:25 +0900

    create kanjinizedDate

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
     <div v-for="(item, index) in items" :key="index">
       {{ item.author }}
       {{ item.text }}
-      {{ item.createdAt }}
+      {{ dateKanjinizer(item.createdAt) }}
     </div>
   </div>
 </template>
@@ -25,9 +25,49 @@ export default {
   },
   methods: {
     addItem(author, text) {
-      this.items.push({ author, text, createdAt: Date() })
+      this.items.push({ author, text, createdAt: new Date() })
       this.newPost.author = ''
       this.newPost.text = ''
+    },
+    dateKanjinizer(createdAt) {
+      const kanjinizedNum = [
+        '〇',
+        '一',
+        '二',
+        '三',
+        '四',
+        '五',
+        '六',
+        '七',
+        '八',
+        '九'
+      ]
+      const year = createdAt.getFullYear()
+      const splittedYear = year.toString().split('')
+      const kanjinizedYear = splittedYear
+        .map((x) => kanjinizedNum[parseInt(x, 10)])
+        .join('')
+      function kanjinizedDoubleDigit(num) {
+        const tensPlace = Math.floor(num / 10)
+        const onesPlace = num % 10
+        if (tensPlace === 0) {
+          return kanjinizedNum[num]
+        }
+        if (tensPlace === 1) {
+          return '十' + kanjinizedNum[onesPlace]
+        }
+        return kanjinizedNum[tensPlace] + '十' + kanjinizedNum[onesPlace]
+      }
+      const month = createdAt.getMonth()
+      const kanjinizedMonth = kanjinizedDoubleDigit(month)
+      const date = createdAt.getDate()
+      const kanjinizedDate = kanjinizedDoubleDigit(date)
+      const hour = createdAt.getHours()
+      const kanjinizedHour = kanjinizedDoubleDigit(hour)
+      const minute = createdAt.getMinutes()
+      const kanjinizedMinute = kanjinizedDoubleDigit(minute)
+      const kanjinizedFullDate = `${kanjinizedYear}年${kanjinizedMonth}月${kanjinizedDate}日${kanjinizedHour}時${kanjinizedMinute}分`
+      return kanjinizedFullDate
     }
   }
 }
